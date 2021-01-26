3dd338f913d54a62898e5334b66c9284d557c130
swy20190 <K201410109@163.com>
2021-01-26 11:15:11 +0800

    filename normalize tool

---
+++ code/filename_normalizer.py
@@ -0,0 +1,14 @@
+# This script normalize the filename of RGB images
+import os
+
+path = input('please enter file path with / at the end: ')
+
+fileList = os.listdir(path)
+
+n = 0
+for i in fileList:
+    oldName = path + os.sep + fileList[n]
+    newName = path + os.sep + 'ADL_'+str(n+1)+'.png'
+    os.rename(oldName, newName)
+    print(oldName, '=====>', newName)
+    n += 1
