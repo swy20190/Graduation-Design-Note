6af1352c72ceda420270b8e89dc1688066d0c82a
swy20190 <K201410109@163.com>
2021-01-26 10:23:16 +0800

    optimize IO of dnn net, delete draw image

--- code/img_parser.py
+++ code/img_parser.py
@@ -3,11 +3,10 @@ import cv2
 import numpy as np
 
 
-def parse(protoFile, weightsFile, nPoints, image):
+def parse(net, nPoints, image):
     """
     parse RGB image into joints coordinate data
-    :param protoFile: prototxt
-    :param weightsFile: trained model
+    :param net: dnn net in memory
     :param nPoints: corresponds to the model
     :param image: RGB file
     :return: a list of (x, y) which is joint coordinate
@@ -15,11 +14,8 @@ def parse(protoFile, weightsFile, nPoints, image):
     POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
                   [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
 
-    # Read the network into memory
-    net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
     # Read image
     frame = cv2.imread(image)
-    frameCopy = np.copy(frame)
     frameWidth = frame.shape[1]
     frameHeight = frame.shape[0]
     threshold = 0.1
@@ -44,26 +40,10 @@ def parse(protoFile, weightsFile, nPoints, image):
         x = (frameWidth * point[0]) / W
         y = (frameHeight * point[1]) / H
         if prob > threshold:
-            cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
             # add thr point to the list if the probability is greater than the threshold
-            points.append((int(x), int(y)))
+            points.append((x, y))
         else:
             points.append(None)
 
-    # draw
-    for pair in POSE_PAIRS:
-        partA = pair[0]
-        partB = pair[1]
-
-        if points[partA] and points[partB]:
-            cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
-            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.circle(frame, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
-    cv2.imshow('Output-Keypoints', frameCopy)
-    cv2.imshow('Output-Skeleton', frame)
-    cv2.imwrite('Output-Keypoints.png', frameCopy)
-    cv2.imwrite('Output-Skeleton.png', frame)
     cv2.waitKey(0)
     return points
--- code/raw_generator.py
+++ code/raw_generator.py
@@ -1,8 +1,11 @@
 # This file generates raw joint coordinate data
 from img_parser import parse
+import cv2
 
 protoFile = "models/pose_deploy_linevec.prototxt"
 weightsFile = "models/pose_iter_440000.caffemodel"
+# Read the network into memory
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 nPoints = 18
-line = parse(protoFile, weightsFile, nPoints, "images/test.png")
+line = parse(net, nPoints, "images/test.png")
 print(line)
