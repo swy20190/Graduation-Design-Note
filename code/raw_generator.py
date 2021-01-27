6e32ce6d04f2e2bfb227244cc04ca5aed2c24fbc
swy20190 <K201410109@163.com>
2021-01-27 10:28:13 +0800

    preprocessing to raw finished

--- code/raw_generator.py
+++ code/raw_generator.py
@@ -11,29 +11,29 @@ weightsFile = "models/pose_iter_440000.caffemodel"
 net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 nPoints = 18
 
-# ADLImgNum = len(os.listdir("images/ADL"))
-# # print(ADLImgNum)
-# ADLRawList = []
-# for i in range(ADLImgNum):
-#     print("processing " + str(i) + "\n")
-#     image_path = "images/ADL/ADL_" + str(i+1) + ".png"
-#     tmp_line = parse(net, nPoints, image_path)
-#     flat_line = []
-#     for coord in tmp_line:
-#         if coord:
-#             flat_line.append(coord[0])
-#             flat_line.append(coord[1])
-#         else:
-#             flat_line.append(None)
-#             flat_line.append(None)
-#     ADLRawList.append(flat_line)
-#
-# ADLRawDf = pd.DataFrame(ADLRawList, columns=['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '4x', '4y', '5x', '5y',
-#                                              '6x', '6y', '7x', '7y', '8x', '8y', '9x', '9y', '10x', '10y', '11x', '11y',
-#                                              '12x', '12y', '13x', '13y', '14x', '14y', '15x', '15y', '16x', '16y',
-#                                              '17x', '17y'])
-# # write the data into csv file
-# ADLRawDf.to_csv("data/ADLRaw.csv", encoding="utf-8")
+ADLImgNum = len(os.listdir("images/ADL"))
+# print(ADLImgNum)
+ADLRawList = []
+for i in range(ADLImgNum):
+    print("processing " + str(i) + "\n")
+    image_path = "images/ADL/ADL_" + str(i+1) + ".png"
+    tmp_line = parse(net, nPoints, image_path)
+    flat_line = []
+    for coord in tmp_line:
+        if coord:
+            flat_line.append(coord[0])
+            flat_line.append(coord[1])
+        else:
+            flat_line.append(None)
+            flat_line.append(None)
+    ADLRawList.append(flat_line)
+
+ADLRawDf = pd.DataFrame(ADLRawList, columns=['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '4x', '4y', '5x', '5y',
+                                             '6x', '6y', '7x', '7y', '8x', '8y', '9x', '9y', '10x', '10y', '11x', '11y',
+                                             '12x', '12y', '13x', '13y', '14x', '14y', '15x', '15y', '16x', '16y',
+                                             '17x', '17y'])
+# write the data into csv file
+ADLRawDf.to_csv("data/ADLRaw.csv", encoding="utf-8")
 
 FallImgNum = len(os.listdir("images/fall"))
 FallRawList = []
