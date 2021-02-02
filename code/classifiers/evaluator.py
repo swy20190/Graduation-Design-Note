bc06468e7e7bc2ede83a3f5d7a99fd572e34dc93
swy20190 <K201410109@163.com>
2021-02-02 10:14:33 +0800

    reconstruct

--- code/classifiers/evaluator.py
+++ code/classifiers/evaluator.py
@@ -6,53 +6,82 @@ from sklearn.metrics import roc_curve
 
 from matplotlib import pyplot as plt
 
-# generate training set
-ADL_train = pd.read_csv("../data/normalized/ADLNormThigh.csv", index_col=0)
-ADL_train = ADL_train[['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '5x', '5y', '6x', '6y', '8x', '8y', '9x', '9y',
-                       '11x', '11y', '12x', '12y']]
-ADL_train = np.array(ADL_train).tolist()
-false_num = len(ADL_train)
 
-Fall_train = pd.read_csv("../data/normalized/FallNormThigh.csv", index_col=0)
-Fall_train = Fall_train[['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '5x', '5y', '6x', '6y', '8x', '8y', '9x', '9y',
-                         '11x', '11y', '12x', '12y']]
-Fall_train = np.array(Fall_train).tolist()
-true_num = len(Fall_train)
+def roc_drawer(features=None, normalized_mode="thigh_len"):
+    """
+    this function generates the plot for roc curve, models are svm based on different normalized data
+    :param features: features chosen to train the model
+    :param normalized_mode: how the data is normalized, e.g. "thigh_len"
+    :return: plot TP rate, FP rate
+    """
+    if features is None:
+        features = ['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '5x', '5y', '6x', '6y', '8x', '8y', '9x', '9y',
+                    '11x', '11y', '12x', '12y']
 
-training_set = ADL_train + Fall_train
-training_label = []
+    # read set
+    ADL_train = None
+    Fall_train = None
+    ADL_test = None
+    Fall_test = None
+    if normalized_mode == "thigh_len":
+        ADL_train = pd.read_csv("../data/normalized/ADLNormThigh.csv", index_col=0)
+        Fall_train = pd.read_csv("../data/normalized/FallNormThigh.csv", index_col=0)
+        ADL_test = pd.read_csv("../data/normalized/ADLNormThighTest.csv", index_col=0)
+        Fall_test = pd.read_csv("../data/normalized/FallNormThighTest.csv", index_col=0)
+    elif normalized_mode == "torso_box":
+        ADL_train = pd.read_csv("../data/normalized/ADLNormTorso.csv", index_col=0)
+        Fall_train = pd.read_csv("../data/normalized/FallNormTorso.csv", index_col=0)
+        ADL_test = pd.read_csv("../data/normalized/ADLNormTorsoTest.csv", index_col=0)
+        Fall_test = pd.read_csv("../data/normalized/FallNormTorsoTest.csv", index_col=0)
+    else:
+        ADL_train = pd.read_csv("../data/normalized/ADLNormNone.csv", index_col=0)
+        Fall_train = pd.read_csv("../data/normalized/FallNormNone.csv", index_col=0)
+        ADL_test = pd.read_csv("../data/normalized/ADLNormNoneTest.csv", index_col=0)
+        Fall_test = pd.read_csv("../data/normalized/FallNormNoneTest.csv", index_col=0)
 
-for i in range(false_num):
-    training_label.append(0)
-for i in range(true_num):
-    training_label.append(1)
+    # generate the training set
+    ADL_train = ADL_train[features]
+    ADL_train = np.array(ADL_train).tolist()
+    false_num = len(ADL_train)
 
-# generate test set
-ADL_test = pd.read_csv("../data/normalized/ADLNormThighTest.csv", index_col=0)
-ADL_test = ADL_test[['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '5x', '5y', '6x', '6y', '8x', '8y', '9x', '9y',
-                    '11x', '11y', '12x', '12y']]
-ADL_test = np.array(ADL_test).tolist()
-false_num = len(ADL_test)
+    Fall_train = Fall_train[features]
+    Fall_train = np.array(Fall_train).tolist()
+    true_num = len(Fall_train)
 
-Fall_test = pd.read_csv("../data/normalized/FallNormThighTest.csv", index_col=0)
-Fall_test = Fall_test[['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '5x', '5y', '6x', '6y', '8x', '8y', '9x', '9y',
-                       '11x', '11y', '12x', '12y']]
-Fall_test = np.array(Fall_test).tolist()
-true_num = len(Fall_test)
+    training_set = ADL_train + Fall_train
+    training_label = []
 
-test_set = ADL_test + Fall_test
-test_label = []
+    for i in range(false_num):
+        training_label.append(0)
+    for i in range(true_num):
+        training_label.append(1)
 
-for i in range(false_num):
-    test_label.append(0)
-for i in range(true_num):
-    test_label.append(1)
+    # generate test set
+    ADL_test = ADL_test[features]
+    ADL_test = np.array(ADL_test).tolist()
+    false_num = len(ADL_test)
 
-svc_thigh = svm.SVC()
-svc_thigh.fit(training_set, training_label)
-thigh_score = svc_thigh.decision_function(test_set)
+    Fall_test = Fall_test[features]
+    Fall_test = np.array(Fall_test).tolist()
+    true_num = len(Fall_test)
 
-fpr, tpr, thresholds = roc_curve(test_label, thigh_score)
+    test_set = ADL_test + Fall_test
+    test_label = []
+
+    for i in range(false_num):
+        test_label.append(0)
+    for i in range(true_num):
+        test_label.append(1)
+
+    svc = svm.SVC()
+    svc.fit(training_set, training_label)
+    scores = svc.decision_function(test_set)
+    fpr, tpr, thresholds = roc_curve(test_label, scores)
+
+    return fpr, tpr
+
+
+fpr, tpr = roc_drawer()
 
 plt.plot(fpr, tpr)
 plt.xlim([0.0, 1.05])
