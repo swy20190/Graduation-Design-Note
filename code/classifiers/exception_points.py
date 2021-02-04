02afa8150773dd1131a3f5a198ece6fa622bb717
swy20190 <K201410109@163.com>
2021-02-04 11:47:31 +0800

    novelty detection

---
+++ code/classifiers/exception_points.py
@@ -0,0 +1,45 @@
+# This file classify data using novelty and outlier detection
+import numpy as np
+import pandas as pd
+from sklearn import svm
+from sklearn.metrics import confusion_matrix
+
+# generate training set
+# note: all the training set must be ADL
+ADL_df = pd.read_csv("../data/normalized/ADLNormThigh.csv")
+# select features
+training_df = ADL_df[['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '5x', '5y', '6x', '6y', '8x', '8y', '9x', '9y',
+                      '11x', '11y', '12x', '12y']]
+training_data = np.array(training_df).tolist()
+
+# fit the model
+clf = svm.OneClassSVM(kernel='rbf', nu=0.01)
+clf.fit(training_data)
+
+# generate the test set
+ADLTest_df = pd.read_csv("../data/normalized/ADLNormThighTest.csv")
+ADLTest_df = ADLTest_df[['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '5x', '5y', '6x', '6y', '8x', '8y', '9x', '9y',
+                         '11x', '11y', '12x', '12y']]
+ADLTest_data = np.array(ADLTest_df).tolist()
+
+FallTest_df = pd.read_csv("../data/normalized/FallNormThighTest.csv")
+FallTest_df = FallTest_df[['0x', '0y', '1x', '1y', '2x', '2y', '3x', '3y', '5x', '5y', '6x', '6y', '8x', '8y', '9x', '9y',
+                           '11x', '11y', '12x', '12y']]
+FallTest_data = np.array(FallTest_df).tolist()
+
+num_norm = len(ADLTest_data)
+num_novel = len(FallTest_data)
+
+test_label = []
+for i in range(num_norm):
+    test_label.append(1)
+for i in range(num_novel):
+    test_label.append(-1)
+
+test_data = ADLTest_data + FallTest_data
+
+predict_label = clf.predict(test_data)
+metric = confusion_matrix(test_label, predict_label)
+
+print(metric)
+
