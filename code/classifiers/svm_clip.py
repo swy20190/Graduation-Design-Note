3a9b4e7e207981f578433e0a5cfdcb4665511dde
swy20190 <K201410109@163.com>
2021-03-27 09:47:28 +0800

    svm based on clips

---
+++ code/classifiers/svm_clip.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+
+clipData = pd.read_csv('../data/clipPureTrain.csv', index_col=0)
+
+clipData.dropna(inplace=True)
+print(len(clipData))
+
+trainSet = clipData[['nx', 'ny', 'rsx', 'rsy', 'lsx', 'lsy', 'rhx', 'rhy', 'lhx', 'lhy', 'rkx', 'rky', 'lkx', 'lky']]
+
+
+labels = clipData['label']
+
+svc = svm.SVC(kernel='rbf')
+
+scores = cross_val_score(svc, trainSet, labels, cv=10)
+print(scores)
